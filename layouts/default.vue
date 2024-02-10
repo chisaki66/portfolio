895ca1a4f55e41a1176ca029af5e6882f12a45ee
Chisaki Ibe <chisaki@ChisakinoMacBook-Pro.local>
2024-02-10 20:52:47 +0900

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <nav class="sidebar">sidebar</nav>
+    <slot />
+  </div>
+</template>
+
+<style lang="scss">
+.sidebar {
+  float: left;
+}
+</style>
---
+++ pages/career.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>career</h1>
+</template>
